d9dc8e86f928afc587ce053783f54824c5fe86ce
Suzanne Dufault <sdufault15@gmail.com>
2017-12-19 15:16:25 -0600

    Dropping "X" column from dataRAND

--- cr_tnd/munge/01-Global-Variables.R
+++ cr_tnd/munge/01-Global-Variables.R
@@ -3,4 +3,4 @@
 period1 <- c("03_05", "05_06", "06_07", "07_08", "08_10", "10_11", "11_12", "12_13", "13_14")
 zstar <- qnorm(0.975)
 
-dataRAND <- Random10000Allocations
+dataRAND <- Random10000Allocations %>% select(-X)
