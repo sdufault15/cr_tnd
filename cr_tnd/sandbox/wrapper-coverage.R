c8b125ca85f8e8e2980c57d0060be3129fcd3ede
Suzanne Dufault <sdufault15@gmail.com>
2017-12-20 16:38:06 -0600

    Updated tables - still waiting on code to finish running

---
+++ cr_tnd/sandbox/wrapper-coverage.R
@@ -0,0 +1,12 @@
+# wrapper
+
+setwd("~/cr_tnd/dec17/")
+period1 <- c("03_05", "05_06", "06_07", "07_08", "08_10", "10_11", "11_12", "12_13", "13_14")
+zstar <- qnorm(0.975)
+
+source("~/cr_tnd/paperFunction2.R")
+source("~/cr_tnd/quadraticFunction.R")
+source("~/cr_tnd/txtSetFunction.R")
+load("~/cr_tnd/Random10000Allocations.RData")
+dataRAND <- Random10000Allocations
+source("~/cr_tnd/coverage-direct-or.R")
