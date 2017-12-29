bd6f3c2050804f0fc4a625abd371181f6b4f32bb
Suzanne Dufault <sdufault15@gmail.com>
2017-12-29 13:08:14 -0600

    Cleaning repo

--- cr_tnd/munge/01-Global-Variables.R
+++
@@ -1,6 +0,0 @@
-## Global
-
-period1 <- c("03_05", "05_06", "06_07", "07_08", "08_10", "10_11", "11_12", "12_13", "13_14")
-zstar <- qnorm(0.975)
-
-dataRAND <- Random10000Allocations %>% select(-X)
--- cr_tnd/munge/README.md
+++
@@ -1,1 +0,0 @@
-This repository contains the preprocessing code for the analysis.
