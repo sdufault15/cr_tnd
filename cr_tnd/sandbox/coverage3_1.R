4afcbe5dc920e3302788f3f9890c2ce165349fca
Suzanne Dufault <sdufault15@gmail.com>
2018-11-04 16:55:48 -0800

    Updating coverage estimates for GEE

--- cr_tnd/sandbox/coverage3_1.R
+++ cr_tnd/sandbox/coverage3_1.R
@@ -1,4 +1,4 @@
 # Coverage 3
 set.seed(123242)
 r.est3.1 <- ORTestFunction(dataRAND, rrIN = 0.3, period1, n1 = 1000, ratio = 1)
-save(r.est3.1, file = "r_est3_12222017.RData")
+save(r.est3.1, file = "r_est3_11042018.RData")
